b2035509e2d44d01b228a380ea7608cd2a56ee3c
yashaswini-mn-26 <yashaswinimn26@gmail.com>
2026-03-06 13:27:26 +0530

    new commit

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -14,7 +14,7 @@ const ForgotPassword: React.FC = () => {
     setError('');
 
     try {
-      const response = await fetch( 'https://e-studyy.vercel.app/api/auth/forgot-password', {
+      const response = await fetch( 'https://e-study-backend.onrender.com/api/auth/forgot-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
